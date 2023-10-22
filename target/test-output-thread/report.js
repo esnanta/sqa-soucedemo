e18b57b18373536895ab92755aac0b3c1c1a833d
Nanta Es <ombakrinai@gmail.com>
2023-10-22 22:30:32 +0700

    Add files via upload
    upload target file

---
+++ target/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"80a67dd7-b5f0-4a4f-8a2d-24b0f4995282","feature":"AddToCart Functionality","scenario":"User add item to cart and remove it","start":1697988065346,"group":1,"content":"","tags":"","end":1697988073844,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
